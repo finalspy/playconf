287477eeb33391244237a2b3e5a7daeb074ed062
Yann PETIT <yann.petit@gmail.com>
2011-05-28 16:38:37 +0200

    Changements mise en forme

--- public/javascripts/playconf.js
+++ public/javascripts/playconf.js
@@ -1,11 +1,11 @@
 $(document).ready(function()
 {
   //hide the all of the element with class msg_body
-  $(".block").hide();
+  $(".blockContent").hide();
   //toggle the componenet with class msg_body
   $(".collapse").click(function()
   {
-    $(this).parent().next(".block").slideToggle(600);
+    $(this).parent().next(".blockContent").slideToggle(400);
     $(this).text() == "[+]"?$(this).text("[-]"):$(this).text("[+]");
   });
 });
